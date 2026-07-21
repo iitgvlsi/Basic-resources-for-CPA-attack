337b569f2a9463438f6d77ddec0aad6e859b628e
iitgvlsi <iitgvlsilab@gmail.com>
2026-07-21 23:35:18 +0530

    Update primetime_script.tcl

--- ASIC Implementation/3. Post layout/5.Power and trace sample collection using PrimeTime PX/primetime_script.tcl
+++ ASIC Implementation/3. Post layout/5.Power and trace sample collection using PrimeTime PX/primetime_script.tcl
@@ -1,43 +1,43 @@
 
  #############################################################
- ##                 Synopsys Primetime                       		               ##
+ ##                 Synopsys Primetime                      ##
  #############################################################
 
  #############################################################
- ##                 Set the power analysis mode                                                  ##
+ ##                 Set the power analysis mode             ##
  #############################################################
 
 set power_enable_analysis TRUE
 set power_analysis_mode time_based
 
  #############################################################
- ##                 Link design		                                                  ##
+ ##                 Link design		                           ##
  #############################################################
 
-set target_library "/home/UMC65/UMK65LSCLLMVBBR_B03_TAPEOUTKIT/synopsys/uk65lscllmvbbr_108c125_wc.db"
+set target_library "uk65lscllmvbbr_108c125_wc.db"
 set link_library "$target_library *"
 read_db $target_library
-read_verilog AES_Composite_enc.v
+read_verilog AES_Composite_enc_netlist.v
 current_design AES_Composite_enc
 link
 
  #############################################################
- ##                 Set transition time/ annotate parasitics                                    ##
+ ##        Set transition time/ annotate parasitics         ##
  #############################################################
 
 
  #############################################################
- ##                 Read swtiching activity file                                                      ##
+ ##              Read swtiching activity file               ##
  #############################################################
 
 read_sdc syn_constraints.sdc 
-read_vcd aes_unprotected.vcd -strip_path testbench/g1
+read_vcd aes_unprotected.vcd -strip_path testbench/g1       ## aes_unprotected.vcd is a file generated using VCS (step 4)
 
  #############################################################
- ##                 Analyze power		                                                  ##
+ ##                  Analyze power		                        ##
  #############################################################
 
-set_power_analysis_options -waveform_format fsdb -waveform_output aes_unprotected_power.fsdb
+set_power_analysis_options -waveform_format fsdb -waveform_output aes_unprotected_power.fsdb ## aes_unprotected_power.fsdb is the output file of this step
 update_power
 
 
